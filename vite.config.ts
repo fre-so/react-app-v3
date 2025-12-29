ba6497a64a4e77a29e959325b78d3039cd51f0b0
jingfelix <jingfelix@outlook.com>
2025-12-29 17:10:44 +0800

    fix: add alias
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
@@ -10,4 +11,9 @@ export default defineConfig({
     allowedHosts: [".e2b.app", ".bespoker.ai"],
   },
   envPrefix: ["VITE_", "MAPBOX_"],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
